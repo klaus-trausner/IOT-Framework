7804c62e1f278ed453c015b0db8757f96f734b5a
Klaus Trausner <trausnerk@gmail.com>
2026-09-25 16:50:15 +0200

    update ota_test.py

--- ota_test.py
+++ ota_test.py
@@ -1,3 +1,3 @@
 # Das ist ein OTA Test
 
-# https://github.com/klaus-trausner/IOT-Framework/blob/90615073929cada516feb5a899b93152da9f1169/ota_test.py
+# https://github.com/klaus-trausner/IOT-Framework/blob/master/ota_test.py
